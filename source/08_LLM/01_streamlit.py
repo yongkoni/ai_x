051c73b02ab781cf8307064696df97c2e4d6c2f4
yongkoni <wndydrhs@naver.com>
2025-06-26 19:12:07 +0900

    2025_06_26

---
+++ source/08_LLM/01_streamlit.py
@@ -0,0 +1,9 @@
+# ctrl+shift+p : 인터프리터 선택
+import streamlit as st
+
+st.set_page_config(page_title="첫번째 프로그램")
+
+st.title("나의 첫 strealit 앱")
+st.subheader("웹 앱을 만들기 위한 강력한 라이브러리 : streamlit")
+
+st.info("text 출력")
